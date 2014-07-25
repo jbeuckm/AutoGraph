49d7fdb6266a2c3887bacc1f3e7f0530a15eea9b
joe beuckman <joebeuckman@gmail.com>
2014-07-24 23:42:41 -0500

    iterating over components and terminals in the components

--- app/autograph-controller.js
+++ app/autograph-controller.js
@@ -1,3 +1,19 @@
 angular.module('AutoGraph').controller('AutographController', ['$scope', function($scope) {
 
+    $scope.placedComponents = [
+        {
+            x: 100,
+            y: 50,
+            inputs: [
+                'in1',
+                'in2'
+            ],
+            outputs: [
+                'out1',
+                'out2',
+                'out3'
+            ]
+        }
+    ];
+
 }]);
---
+++ app/base-component/base-component-directive.js
@@ -0,0 +1,24 @@
+angular.module('AutoGraph').directive('component', function(){
+
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'base-component/base-component-template.svg',
+
+        link: function (scope, elem, attrs) {
+            // Extract the child text element to replace the SVG element.
+            var child = angular.element(elem[0].firstElementChild);
+
+            // Copy properties over.
+            for (attrName in attrs) {
+                if(typeof attrs[attrName] === "string") {
+                    child.attr(attrName, attrs[attrName]);
+                }
+            }
+
+            elem.replaceWith(child);
+        }
+
+    };
+
+});
