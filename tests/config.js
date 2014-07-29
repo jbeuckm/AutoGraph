ef73c6396a7eee53d82c7963eedb0881e014fa22
joe <joseph.beuckman@usbank.com>
2014-07-29 13:23:17 -0500

    establishing travis

--- tests/config.js
+++ tests/config.js
@@ -1,4 +1,21 @@
-exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+
+if (process.env.TRAVIS) {
+
+    exports.config = {
+        sauceUser: process.env.SAUCE_USERNAME,
+        sauceKey: process.env.SAUCE_ACCESS_KEY,
+        capabilities: {
+            'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+            'build': process.env.TRAVIS_BUILD_NUMBER
+        }
+    };
+
+} else {
+
+    exports.config = {
+        seleniumAddress: 'http://localhost:4444/wd/hub',
+        specs: ['spec.js']
+    };
+
 }
+
