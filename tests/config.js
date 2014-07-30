8e4087d65b4156181c5c6d49d32fa0a370285742
joe beuckman <joebeuckman@gmail.com>
2014-07-29 23:26:26 -0500

    corrected to protractor config file

--- tests/config.js
+++ tests/config.js
@@ -1,21 +1,6 @@
 
-if (process.env.TRAVIS) {
-
-    exports.config = {
-        sauceUser: process.env.SAUCE_USERNAME,
-        sauceKey: process.env.SAUCE_ACCESS_KEY,
-        capabilities: {
-            'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-            'build': process.env.TRAVIS_BUILD_NUMBER
-        }
-    };
-
-} else {
-
-    exports.config = {
-        seleniumAddress: 'http://localhost:4444/wd/hub',
-        specs: ['spec.js']
-    };
-
-}
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['spec.js']
+};
 
