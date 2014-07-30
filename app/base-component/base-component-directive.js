a9db04e3379a9f68b15906c49361604178ea6a08
joe beuckman <joebeuckman@gmail.com>
2014-07-29 23:22:26 -0500

    toward a draggable base component

--- app/base-component/base-component-directive.js
+++ app/base-component/base-component-directive.js
@@ -1,4 +1,5 @@
-angular.module('AutoGraph').directive('component', function(){
+angular.module('AutoGraph').directive('component', function($document){
+
 
     return {
         restrict: 'E',
@@ -7,12 +8,38 @@ angular.module('AutoGraph').directive('component', function(){
         templateUrl: 'base-component/base-component-template.svg',
 
         link: function (scope, element, attributes) {
-return;
-            // Extract the child text element to replace the SVG element.
-            var child = angular.element(element[0].firstElementChild);
+
+            element.on('mousedown', function(event) {
+                // Prevent default dragging of selected content
+                event.preventDefault();
+                startX = event.pageX - scope.x;
+                startY = event.pageY - scope.y;
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
+
+            console.log(scope);
+            function mousemove(event) {
+                scope.y = event.pageY - startY;
+                scope.x = event.pageX - startX;
+/*
+                element.css({
+                    top: y + 'px',
+                    left:  x + 'px'
+                });
+*/
+            }
+
+            function mouseup() {
+                $document.off('mousemove', mousemove);
+                $document.off('mouseup', mouseup);
+            }
 
             scope.rectWidth = 100;
             scope.rectHeight = 30;
+return;
+            // Extract the child text element to replace the SVG element.
+            var child = angular.element(element[0].firstElementChild);
 /*
             // Copy properties over.
             for (key in attributes) {
