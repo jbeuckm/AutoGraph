ad986d59dacd7fe94cf040e83d227b527c492f99
joe beuckman <joebeuckman@gmail.com>
2014-08-03 14:18:21 -0500

    terminal mouseover resize

--- app/terminal/terminal-directive.js
+++ app/terminal/terminal-directive.js
@@ -15,6 +15,22 @@ angular.module('AutoGraph').directive('terminal', function () {
                 scope.labelDy = 32;
             }
 
+            scope.height = 4;
+            scope.mouseOver = function(e) {
+                console.log('mouseOver');
+                scope.height = 8;
+                if (attributes['type'] == 'input') {
+                    scope.dy = -8;
+                }
+            };
+            scope.mouseOut = function(e) {
+                console.log('mouseOut');
+                scope.height = 4;
+                if (attributes['type'] == 'input') {
+                    scope.dy = -4;
+                }
+            };
+
         }
     };
 
