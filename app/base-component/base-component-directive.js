debcdd42dac57ddfa2d4d4f46cb6af430bc0159d
joe beuckman <joebeuckman@gmail.com>
2014-08-05 00:21:17 -0500

    double timeout solves resize problem for many buttons

--- app/base-component/base-component-directive.js
+++ app/base-component/base-component-directive.js
@@ -18,12 +18,14 @@ angular.module('AutoGraph').directive('component', ['$document', '$compile', '$t
             element.append(componentDirective);
 
             $timeout(function(){
-                var el = document.getElementById(scope.component.uuid);
-                console.log(el);
-                var bb  = el.getBoundingClientRect();
-            console.log(bb);
-                scope.rectWidth = 2 * padding + bb.width;
-                scope.rectHeight = 2 * padding + bb.height;
+                $timeout(function(){
+                    var el = document.getElementById(scope.component.uuid);
+
+                    var bb  = el.getBoundingClientRect();
+
+                    scope.rectWidth = 2 * padding + bb.width;
+                    scope.rectHeight = 2 * padding + bb.height;
+                }, 0);
             }, 0);
 
             element.on('mousedown', function(event) {
