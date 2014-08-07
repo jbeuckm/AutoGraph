6ae9c66cc07dc4e1e34e773c9abaab5c5a29967a
joe beuckman <joebeuckman@gmail.com>
2014-08-06 23:37:00 -0500

    terminal uuid assignment back into autograph-controller where it should be

--- app/terminal/terminal-controller.js
+++ app/terminal/terminal-controller.js
@@ -1,8 +1,5 @@
 angular.module('AutoGraph').controller('TerminalController', ['$scope', 'rfc4122', function($scope, rfc4122) {
 
-    $scope.terminal.uuid = rfc4122.newUuid();
-    $scope.terminalIndex[$scope.terminal.uuid] = this;
-
-    $scope.receiveTick();
+//    $scope.receiveTick();
 
 }]);
