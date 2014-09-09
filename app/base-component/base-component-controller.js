3c84e1ac40629b175ce95eedb6677e5e230bccab
joe beuckman <joebeuckman@gmail.com>
2014-09-08 23:03:41 -0500

    preparing to listen for ticks and values from input terminals

--- app/base-component/base-component-controller.js
+++ app/base-component/base-component-controller.js
@@ -3,6 +3,14 @@
  */
 angular.module('AutoGraph').controller('BaseComponentController', ['$scope', function($scope) {
 
+    console.log('$scope.component');
+    for (var i in $scope.component.inputs) {
+        console.log($scope.component.inputs[i]);
+    }
+    for (var i in $scope.component.outputs) {
+        console.log($scope.component.outputs[i]);
+    }
+
     $scope.generateTick = function(input) {
 console.log('generateTick');
 
