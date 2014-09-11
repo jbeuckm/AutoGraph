af7379bead52b9e293e223fcf22a3706c627934f
joe beuckman <joebeuckman@gmail.com>
2014-09-10 22:43:16 -0500

    testing clear components before wire placement

--- tests/wire-directive.spec.js
+++ tests/wire-directive.spec.js
@@ -38,10 +38,12 @@ describe('wire-directive', function(){
     it('should place and clear wires', function(){
 
         scope.clearComponents();
+        expect(scope.placed.components).toEqual({});
+
         var component1template = {
             uuid: 1,
             outputs: [
-                { 
+                {
                     label: "output"
                 }
             ]
@@ -49,7 +51,7 @@ describe('wire-directive', function(){
         var component2template = {
             uuid: 2,
             inputs: [
-                { 
+                {
                     label: "input"
                 }
             ]
@@ -57,11 +59,11 @@ describe('wire-directive', function(){
         var component1 = scope.placeNewComponent(component1template, 10, 10);
         var component2 = scope.placeNewComponent(component2template, 250, 10);
         expect(scope.placed.components).not.toEqual({});
-        
+
         scope.initiateWire(component1.outputs[0]);
         var wire = scope.completeWire(component2.inputs[0]);
         expect(scope.placed.wires).not.toEqual({});
-        
+
         scope.deleteWire(wire.uuid);
         expect(scope.placed.wires).toEqual({});
     });
